8297edec9e99f1d3b897fc7177ff60d108086dcd
jongmin <ace4463@gmail.com>
2022-06-21 15:40:54 +0900

    modify 125

--- 125.valid_palindrome.py
+++ 125.valid_palindrome.py
@@ -1,12 +1,21 @@
-def isPalindrome(s: str) -> bool:
+import re
+
+def is_palindrome1(s: str) -> bool:
     strs = []
     for char in s:
         if char.isalnum():
             strs.append(char.lower())
+    # 펠린드롬 여부
     while len(strs) > 1:
         if strs.pop(0) != strs.pop():
             return False
     return True
 
-print(isPalindrome('A man, a plan, a canal: Panama'))
+def is_palindrome2(s: str) -> bool:
+    s = s.lower()
+    s = re.sub('[^a-z0-9]', '', s)
+    return s == s[::-1]
+
+print(is_palindrome1('A man, a plan, a canal: Panama'))
+print(is_palindrome2('race a car'))
 
