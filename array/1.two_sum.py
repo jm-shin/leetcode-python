25c8f4e1a0e6aaf0baba86373d7308565573f6fc
jongmin <ace4463@gmail.com>
2022-06-23 16:20:11 +0900

    Add 1.two_sum.py

---
+++ array/1.two_sum.py
@@ -0,0 +1,39 @@
+from typing import List
+
+nums = [2, 7, 11, 15]
+target = 9
+
+
+# 브루트 포스 방식
+def two_sum1(nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+
+# in을 이용한 탐색
+def two_sum2(nums: List[int], target: int) -> List[int]:
+    for i, n in enumerate(nums):
+        complement = target - n
+
+        if complement in nums[i + 1:]:
+            return [nums.index(n), nums[i + 1:].index(complement) + (i + 1)]
+
+
+# 첫 번째 수를 뺀 결과 키 조회
+def two_sum3(nums: List[int], target: int) -> List[int]:
+    nums_map = {}
+    # 키와 값을 바꿔서 딕셔너리로 저장
+    for i, num in enumerate(nums):
+        nums_map[num] = i
+
+    # 타겟에서 첫 번째 수를 뺀 결과를 키로 조회
+    for i, num in enumerate(nums):
+        if target - num in nums_map and i != nums_map[target - num]:
+            return [i, nums_map[target - num]]
+
+
+print(two_sum1(nums, target))
+print(two_sum2(nums, target))
+print(two_sum3(nums, target))
