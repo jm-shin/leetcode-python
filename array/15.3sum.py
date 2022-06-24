6f71fb5970a1c7651ff9bb81198dfa3ad02c7f97
jongmin <ace4463@gmail.com>
2022-06-24 18:07:55 +0900

    Add 15.3sum.py

---
+++ array/15.3sum.py
@@ -0,0 +1,37 @@
+from typing import List
+
+nums = [-1, 0, 1, 2, -1, -4]
+
+
+# 투포인터 방식
+def three_sum(nums: List[int]) -> List[List[int]]:
+    results = []
+    nums.sort()
+
+    for i in range(len(nums) - 2):
+        # 중복된 값 건너뛰기
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        # 간격을 좁혀가며 합 'sum' 계산
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            sum = nums[i] + nums[left] + nums[right]
+            if sum < 0:
+                left += 1
+            elif sum > 0:
+                right -= 1
+            else:
+                # `sum = 0`인 경우 장답 및 스킵 처리
+                results.append([nums[i], nums[left], nums[right]])
+
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+    return results
+
+
+print(three_sum(nums))
